2b20965b81a46080dc3d549a784f0c70d692834d
semihalev <semihalev@gmail.com>
2018-10-03 13:49:35 +0300

    clean code

--- resolver.go
+++ resolver.go
@@ -33,77 +33,9 @@ var roothints = []string{
 	"202.12.27.33:53",
 }
 
-// Lookup will ask each nameserver in top-to-bottom fashion, starting a new request
-// in every second, and return as early as possbile (have an answer).
+// Resolve will ask each nameserver in top-to-bottom fashion, starting a new request
+// in every interval, and return as early as possbile (have an answer).
 // It returns an error if no request has succeeded.
-func (r *Resolver) Lookup(Net string, req *dns.Msg) (message *dns.Msg, err error) {
-	c := &dns.Client{
-		Net:          Net,
-		UDPSize:      dns.DefaultMsgSize,
-		Dialer:       &net.Dialer{Timeout: time.Duration(Config.ConnectTimeout) * time.Second},
-		ReadTimeout:  time.Duration(Config.Timeout) * time.Second,
-		WriteTimeout: time.Duration(Config.Timeout) * time.Second,
-	}
-
-	qname := req.Question[0].Name
-	qtype := dns.Type(req.Question[0].Qtype).String()
-
-	res := make(chan *dns.Msg)
-
-	var wg sync.WaitGroup
-
-	L := func(nameserver string) {
-		defer wg.Done()
-
-		r, _, err := c.Exchange(req, nameserver)
-		if err != nil && err != dns.ErrTruncated {
-			log.Debug("Got an error from resolver", "qname", qname, "qtype", qtype, "resolver", nameserver, "net", Net, "error", err.Error())
-			return
-		}
-
-		if r != nil && r.Rcode != dns.RcodeSuccess {
-			log.Debug("Failed to get a valid answer", "qname", qname, "qtype", qtype, "resolver", nameserver, "net", Net)
-			if r.Rcode == dns.RcodeServerFailure && req.Question[0].Qtype != dns.TypePTR {
-				return
-			}
-		} else {
-			log.Debug("Resolve query", "qname", unFqdn(qname), "qtype", qtype, "resolver", nameserver, "net", Net)
-		}
-
-		select {
-		case res <- r:
-		default:
-		}
-	}
-
-	ticker := time.NewTicker(time.Duration(Config.Interval) * time.Millisecond)
-	defer ticker.Stop()
-
-	// Start lookup on each nameserver top-down, in every second
-	for _, nameserver := range Config.Nameservers {
-		wg.Add(1)
-		go L(nameserver)
-
-		// but exit early, if we have an answer
-		select {
-		case r := <-res:
-			return r, nil
-		case <-ticker.C:
-			continue
-		}
-	}
-
-	// wait for all the namservers to finish
-	wg.Wait()
-	select {
-	case r := <-res:
-		return r, nil
-	default:
-		return nil, fmt.Errorf("resolv failed")
-	}
-}
-
-// Resolve func
 func (r *Resolver) Resolve(net string, req *dns.Msg, servers []string, root bool, depth int) (resp *dns.Msg, err error) {
 	if depth == 0 {
 		return resp, fmt.Errorf("maximum recursion depth for DNS tree queried")
@@ -247,7 +179,7 @@ func (r *Resolver) lookup(Net string, req *dns.Msg, servers []string) (resp *dns
 
 		r, _, err := c.Exchange(req, server)
 		if err != nil && err != dns.ErrTruncated {
-			log.Debug("Got an error from resolver", "qname", qname, "qtype", qtype, "server", server, "net", Net, "error", err.Error())
+			log.Info("Got an error from resolver", "qname", qname, "qtype", qtype, "server", server, "net", Net, "error", err.Error())
 			return
 		}
 
