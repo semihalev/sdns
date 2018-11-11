0a1f609df52d8f28b6512536c8cd041729838d9e
semihalev <semihalev@gmail.com>
2018-11-11 19:57:05 +0000

    update doc

--- doc.go
+++ doc.go
@@ -1,1 +1,4 @@
-package main
+/*
+A Lightweight fast recursive dns server with dnssec support. https://semihalev.github.io/sdns/ for more information.
+*/
+package main // import "github.com/semihalev/sdns"
