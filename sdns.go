e1386f04d95731c2f64d5de3f624c4cd1a8cd537
semihalev <semihalev@gmail.com>
2018-11-12 17:23:52 +0000

    release version 0.2.3

--- sdns.go
+++ sdns.go
@@ -23,7 +23,7 @@ var (
 	Config *config.Config
 
 	// Version returns the build version of sdns, this should be incremented every new release
-	Version = "0.2.3-rc3"
+	Version = "0.2.3"
 
 	// ConfigVersion returns the version of sdns, this should be incremented every time the config changes so sdns presents a warning
 	ConfigVersion = "0.2.3"
