c8cc30010c062422d524f355c4bcf39f200bfe76
semihalev <semihalev@gmail.com>
2020-05-15 21:37:53 +0300

    revert back uncompatibility

--- response/typify.go
+++ response/typify.go
@@ -106,8 +106,7 @@ func Typify(m *dns.Msg, t time.Time) (Type, *dns.OPT) {
 	}
 
 	if !soa && len(m.Question) > 0 {
-		if len(m.Answer) == 0 &&
-			(m.Question[0].Qtype == dns.TypeDNSKEY || m.Question[0].Qtype == dns.TypeDS) {
+		if len(m.Answer) == 0 && m.Question[0].Qtype == dns.TypeDNSKEY {
 			return NoCache, opt
 		}
 	}
