62a243b6e56a0ee1c580e32d2599ad6ec637ca59
semihalev <semihalev@gmail.com>
2020-07-08 14:06:09 +0300

    build.sh update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 go get -v github.com/mitchellh/gox
-${HOME}/go/bin/gox -osarch="linux/amd64 netbsd/amd64 darwin/amd64 freebsd/amd64 windows/amd64" -output="sdns_{{.OS}}_{{.Arch}}" -ldflags="-s -w"
+${HOME}/go/bin/gox -osarch="linux/amd64 linux/arm linux/arm64 openbsd/amd64 netbsd/amd64 darwin/amd64 freebsd/amd64 windows/amd64" -output="sdns_{{.OS}}_{{.Arch}}" -ldflags="-s -w"
